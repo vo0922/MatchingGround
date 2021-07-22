bf8b695ab00ee8be22596187e2eb04e39737a975
kangjaemingit <71342315+kangjaemingit@users.noreply.github.com>
2021-07-22 15:08:17 +0900

    start

---
+++ src/MainScreen/MainScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainScreen extends Component {
+    render() {
+        return (
+            <div>
+                메인스크린
+            </div>
+        )
+    }
+}
