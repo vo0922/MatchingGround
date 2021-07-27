fefc031bb3cbe6bc511da0faf347fb85be7a1ad5
kangjaemingit <71342315+kangjaemingit@users.noreply.github.com>
2021-07-27 23:40:35 +0900

    20210727 메인, 네비바 임시

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import MainScreen from './MainScreen/MainScreen';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <MainScreen/>
