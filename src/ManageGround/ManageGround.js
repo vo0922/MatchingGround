94cf2dcc45cf9d7ed2090c166be139914217ea33
kangjaemingit <71342315+kangjaemingit@users.noreply.github.com>
2021-08-02 22:47:28 +0900

    테스트

---
+++ src/ManageGround/ManageGround.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+export default class ManageGround extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="md">
+          <Typography component="div" style={{ backgroundColor: "#cfe8fc", height: "100vh" }}>
+
+          </Typography>
+        </Container>
+      </React.Fragment>
+    );
+  }
+}
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -5,6 +5,7 @@ import LoginCallBack from '../Login/Callback'
 import Main from '../MainScreen/MainScreen'
 import PrivateRoute from './PrivateRoute';
 import PrivateRouteL from './PrivateRouteL';
+import ManageGround from '../ManageGround/ManageGround';
 
 const Routes = () => {
     return (
@@ -13,6 +14,7 @@ const Routes = () => {
                 <PrivateRouteL exact path="/login" component={Login}/>
                 <PrivateRouteL exact path="/login/callback" component={LoginCallBack}/>
                 <PrivateRoute exact path="/" component={Main} />
+                <Route exact path="/manageground" component={ManageGround}/>
             </Switch>
         </Router>
     );
