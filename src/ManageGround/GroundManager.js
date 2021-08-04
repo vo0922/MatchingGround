f2b9d8379b3be13c617de51bb9608bc04173a429
kangjaemingit <71342315+kangjaemingit@users.noreply.github.com>
2021-08-04 20:58:06 +0900

    20210804

---
+++ src/ManageGround/GroundManager.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import MainLogo from "../MainScreen/MainHeader/MainLogo";
+import Button from "@material-ui/core/Button";
+
+export default function GroundManager() {
+  return (
+    <React.Fragment>
+      <MainLogo />
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#F3F3F3", height: "80vh" }}
+        >
+          <h2>
+            경기장 관리자가 아닙니다. 경기장을 등록하시려면 경기장 등록 버튼을
+            눌러주세요.
+          </h2>
+          <Button variant="outlined" color="primary">
+            경기장 등록 신청하기
+          </Button>
+        </Typography>
+      </Container>
+    </React.Fragment>
+  );
+}
---
+++ src/Router/PrivateRouteGM.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+  const PrivateRoute = ({component: Component, ...rest}) => { 
+      return (
+          <Route {...rest} render={props => (
+            window.sessionStorage.getItem('id') ?
+                <Component {...props}/>
+                :<Redirect to="/Login" />
+          )} />
+      );
+  };
+
+export default PrivateRoute;
